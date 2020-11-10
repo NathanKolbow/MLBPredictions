a3cd59fbbcc2dcc0a53a71d0214a74ba8566f4d1
Nathan Kolbow <nkolbow@wisc.edu>
2020-11-10 17:02:20 -0600

    knn adjustments & nn_preprocessing.py

--- knn.py
+++ knn.py
@@ -4,17 +4,9 @@ from sklearn.neighbors import KNeighborsClassifier as knn_classifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import Normalizer
 
-# Read in the dataset
-df = pd.read_csv("pitches.csv")
-# We can't use categorical variables in kNN, so we pop them
-to_pop = ["Unnamed: 0", "pitch_type", "stand", "p_throws", "inning_topbot", "pitcher_name", "catcher_name", "umpire_name", "batter_name", "gaaame_date"]
-for item in to_pop:
-    df.pop(to_pop)
-    
-# Split the data into training and testing
-y = df.pop("description").values
-y_lookup, y = np.unique(y, return_inverse=True)
-X = df.values
+# Read and split the data into training and testing
+X = np.load('nn_X.npy')
+y = np.load('nn_y.npy')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, stratify=y)
 
 
@@ -22,7 +14,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, strat
 knn = knn_classifier(n_neighbors=100)
 knn.fit(X_train, y_train)
 
-# Evaluate the model; the accuracy is around 0.5795 depending on the train/test split
+# Evaluate the model; the accuracy is around 0.58 depending on the train/test split
 print(knn.score(X_test, y_test))
 
 
