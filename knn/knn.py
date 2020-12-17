04b854aef5c97e6006d0d1f449f969ed13904f67
Nathan Kolbow <nkolbow@wisc.edu>
2020-12-17 16:18:22 -0600

    Organization

---
+++ knn/knn.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier as knn_classifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Normalizer
+
+# Read and split the data into training and testing
+df = pd.read_csv("pitches.csv")
+del df['Unnamed: 0']
+factor_vars = ["game_date", "pitch_type", "stand", "p_throws", "inning_topbot", "pitcher_name", "catcher_name", "umpire_name", "batter_name"]
+for var in factor_vars:
+    del df[var]
+
+y = df.pop('description').values
+X = df.values
+del df
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, stratify=y)
+
+
+for i in [5, 10, 15, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300]:
+    # Fit the model w/o normalization first; 100 was found to locally maximize prediction accuracy
+    knn = knn_classifier(n_neighbors=i)
+    knn.fit(X_train, y_train)
+
+    # Evaluate the model; the accuracy is around 0.58 depending on the train/test split
+    print("k=%s: %s" % (i, knn.score(X_test, y_test)))
+"""
+    Results:
+        k=5: 0.5456448100081732
+        k=10: 0.5619062017225169
+        k=15: 0.5671266659422584
+        k=25: 0.5731786249155513
+        k=50: 0.5781534202308343
+        k=75: 0.5777849168741467
+        k=100: 0.577288854663221
+        k=125: 0.5770242881507274
+        k=150: 0.5757581484123647
+        k=175: 0.5749030316487691
+        k=200: 0.5745439770960991
+        k=225: 0.5736888603325034
+        k=250: 0.5726778383026169
+        k=275: 0.5718132727350036
+        k=300: 0.5701455588258916
+"""
+
+
+# Fit the model w/ normalization
+norm = Normalizer()
+norm.fit(X_train)
+X_train_norm = norm.transform(X_train)
+X_test_norm = norm.transform(X_test)
+
+for i in [5, 10, 15, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300]:
+    knn = knn_classifier(n_neighbors=i)
+    knn.fit(X_train_norm, y_train)
+    # Evaluate the model
+    print("k=%s: %s" % (i, knn.score(X_test_norm, y_test)))
+"""
+    Results:
+        k=5: 0.5750258661009983
+        k=10: 0.5945045755833456
+        k=15: 0.5996919689890252
+        k=25: 0.6054699126458069
+        k=50: 0.6081911682028848
+        k=75: 0.6077470744140561
+        k=100: 0.6071848705750069
+        k=125: 0.6064053442435524
+        k=150: 0.6061927461531557
+        k=175: 0.6058951088266002
+        k=200: 0.6049502284248371
+        k=225: 0.6044730638219468
+        k=250: 0.6040998360632502
+        k=275: 0.6037313327065627
+        k=300: 0.6032116484855929
+"""
---
+++ neural-nets/nn.py
@@ -0,0 +1,97 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import np_utils
+import keras
+from joblib import dump
+
+
+import tensorflow as tf
+from tensorflow.keras import models,layers
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+
+
+# Source: https://machinelearningmastery.com/multi-class-classification-tutorial-keras-deep-learning-library/
+
+
+class KerasGridOptimizer():
+    def __init__(self, epochs=10, n_layers=1, n_nodes=32, learning_rate=5e-4, batch_size=128):
+        self._epochs = epochs
+        self._n_layers = n_layers
+        self._n_nodes = n_nodes
+        self._learn_rate = learning_rate
+        self._batch_size = batch_size
+        
+    def get_params(self, deep=False):
+        return { 'epochs':self._epochs, 'n_layers':self._n_layers, 'n_nodes':self._n_nodes }
+        
+    def set_params(self, epochs=None, n_layers=None, n_nodes=None, learning_rate=None, batch_size=None):
+        if epochs is not None:
+            self._epochs = epochs
+        if n_layers is not None:
+            self._n_layers = n_layers
+        if n_nodes is not None:
+            self._n_nodes = n_nodes
+        if learning_rate is not None:
+            self._learn_rate = learning_rate
+        if batch_size is not None:
+            self._batch_size = batch_size
+        return self
+        
+    def fit(self, X, y):
+        self._model = models.Sequential()
+        self._model.add(layers.Flatten(input_shape=(26,)))
+        for i in range(self._n_layers):
+            self._model.add(layers.Dense(self._n_nodes, activation='relu'))
+        self._model.add(layers.Dense(3, activation='softmax'))
+            
+        opt = keras.optimizers.Adam(learning_rate=self._learn_rate)
+        self._model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+        
+        self._est = KerasClassifier(build_fn=lambda:self._model, epochs=self._epochs, verbose=0, batch_size=self._batch_size)
+        
+        self._est.fit(X, y)
+        
+    def score(self, X, y):
+        return self._est.score(X, y)
+    
+    
+
+if __name__ == '__main__':
+    df = pd.read_csv("pitches.csv")
+
+    to_pop = ["Unnamed: 0", "pitch_type", "stand", "p_throws", "inning_topbot", "pitcher_name", "catcher_name", "umpire_name", "batter_name", "game_date"]
+    for item in to_pop:
+        df.pop(item)
+
+    # THESE ARE FEATURES THAT ACCOUNTED FOR LESS THAN 1% OF FEATURE IMPORTANCE IN TOTAL AFTER
+    # FITTING THE FIRST "FINAL" GBM MODEL
+    to_pop = ["was_3_0","pitch_type_FF","release_speed","inning","was_2_0","release_extension","outs_when_up","pitch_type_CH","was_3_2","pitch_type_SI","pitch_type_FT","pitch_type_FC","bat_score","post_bat_score","pitch_type_CU","was_3_1","pitch_type_FS","was_1_1","was_0_1","was_2_1","was_1_2","was_2_2","was_0_2","description","zone"]
+    for item in to_pop:
+        df.pop(item)
+    X = df.values
+    y = np.load('nn_y.npy')
+    
+    X = X[0:50000, :]
+    y = y[0:50000, :]
+    
+    model = KerasGridOptimizer()
+    param_gr = dict(epochs=[200, 400], 
+                    n_layers=[2, 8, 32, 64, 128],
+                    n_nodes=[1, 2, 4, 8, 16, 32, 64, 256],
+                    learning_rate=[0.5, 0.001],
+                    batch_size=[256]
+                   )
+    grid = GridSearchCV(estimator=model, param_grid=param_gr, verbose=2, n_jobs=3, cv=3)
+    print("Fitting grid.")
+    grid.fit(X, y)
+    
+    print("Best score: %s" % grid.best_score_)
+    print("Best params: %s" % grid.best_params_)
+    
